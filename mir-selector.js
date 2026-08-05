c5a55cf8186ec1fd2ff49db1eeedac030d463e8a
ElliottIan397 <IanElliott@EandSsolutions.com>
2026-08-05 14:52:15 -0500

    Update mir-selector.js
    Fix syntax error

--- mir-selector.js
+++ mir-selector.js
@@ -4,22 +4,23 @@ const container = document.getElementById("mir-selector");
 
 if (!container) {
     console.log("No MIR selector container found.");
-    return;
+} else {
+
+    container.innerHTML = "<p>Loading states...</p>";
+
+    fetch("https://automation.digitolservices.com/webhook/mir/states")
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+
+            container.innerHTML =
+                "<pre>" + JSON.stringify(data, null, 2) + "</pre>";
+        })
+        .catch(error => {
+            console.error(error);
+
+            container.innerHTML =
+                "<p style='color:red;'>Unable to load states.</p>";
+        });
+
 }
-
-container.innerHTML = "<p>Loading states...</p>";
-
-fetch("https://automation.digitolservices.com/webhook/mir/states")
-    .then(response => response.json())
-    .then(data => {
-        console.log(data);
-
-        container.innerHTML =
-            "<pre>" + JSON.stringify(data, null, 2) + "</pre>";
-    })
-    .catch(error => {
-        console.error(error);
-
-        container.innerHTML =
-            "<p style='color:red;'>Unable to load states.</p>";
-    });
